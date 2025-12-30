21b3b335612923dc6b419462ddfdb9137d3bd988
blu3fishez <kcw2205@naver.com>
2025-12-30 17:45:54 +0900

    feat: RequestHandler 간소화
    - Factory 클래스를 활용하여 RequestHandler 에서 작성되어 있던 메세지 파싱 및 작성 로직을 분리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,76 +1,62 @@
 package webserver;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import webserver.interceptor.RequestInterceptorMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import webserver.http.HttpResponse;
-import webserver.http.HttpResponseFactory;
-import webserver.http.HttpStatus;
+import webserver.data.enums.HttpStatusCode;
+import webserver.data.HttpRequest;
+import webserver.data.HttpResponse;
+import webserver.factory.HttpRequestFactory;
+import webserver.factory.HttpResponseFactory;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private final Socket connection;
-    private DataOutputStream outputStream;
-    private final HttpResponseFactory responseFactory;
+    private final HttpResponseFactory httpResponseFactory;
+    private final HttpRequestFactory httpRequestFactory;
+    private final RequestInterceptorMapper requestInterceptorMapper;
 
-    public RequestHandler(Socket connectionSocket, HttpResponseFactory responseFactory) {
-        this.responseFactory = responseFactory;
+    public RequestHandler(
+            Socket connectionSocket,
+            HttpRequestFactory httpRequestFactory,
+            HttpResponseFactory httpResponseFactory,
+            RequestInterceptorMapper requestInterceptorMapper
+    ) {
+        this.httpResponseFactory = httpResponseFactory;
+        this.httpRequestFactory = httpRequestFactory;
         this.connection = connectionSocket;
-
-//        try (OutputStream out = connection.getOutputStream()) {
-//            outputStream = new DataOutputStream(out);
-//        } catch (IOException e) {
-//            logger.error(e.getMessage());
-//        }
+        this.requestInterceptorMapper = requestInterceptorMapper;
     }
 
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        // TODO: Keep alive 에 대한 옵션이 있는데 리소스를 정리해도 되는가?
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream();) {
 
-            // TODO: 1단계 설계대로 개선 필요
-            DataOutputStream dos = new DataOutputStream(out);
+            HttpRequest httpRequest = httpRequestFactory.parseRequestFromStream(in);
+            HttpResponse httpResponse = null;
 
-            InputStream inputStream = getClass().getClassLoader().getResourceAsStream("static/index.html");
+            httpResponse = requestInterceptorMapper.intercept(httpRequest);
 
-            if (inputStream == null) {
-                throw new IllegalStateException("Resource not found");
+            if (httpResponse == null) {
+                httpResponse = httpResponseFactory
+                        .createResponse(
+                                httpRequest.getHttpVersion(),
+                                HttpStatusCode.INTERNAL_SERVER_ERROR,
+                                "Request Mapping not found",
+                                "text/plain;charset=utf-8");
             }
 
-            byte[] body = inputStream.readAllBytes();
-
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-            inputStream.close();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+            out.write(httpResponse.serialize());
+            out.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
