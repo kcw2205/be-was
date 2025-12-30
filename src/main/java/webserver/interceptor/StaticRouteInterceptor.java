c262197dcc187fd0961d7f51230494b7e4698322
blu3fishez <kcw2205@naver.com>
2025-12-30 17:48:48 +0900

    feat: 특정 서브 페이지 URI에 대한 index.html 라우팅 기능 추가

---
+++ src/main/java/webserver/interceptor/StaticRouteInterceptor.java
@@ -0,0 +1,24 @@
+package webserver.interceptor;
+
+import webserver.data.HttpRequest;
+import webserver.data.ResponseEntity;
+import webserver.data.enums.StaticFileEnum;
+
+public class StaticRouteInterceptor implements RequestInterceptor {
+
+    @Override
+    public ResponseEntity handleRequest(HttpRequest httpRequest) {
+        if (httpRequest.getRequestURI().equals("/"))
+            return handleRouteRequest(httpRequest.getRequestURI() + "index.html");
+        return handleRouteRequest(httpRequest.getRequestURI() + "/index.html");
+    }
+
+    private ResponseEntity handleRouteRequest(String file) {
+        ResponseEntity responseEntity = StaticFileEnum.HTML.fetchStaticFile(file.substring(1));
+        System.out.println(responseEntity.getContentType());
+        if (responseEntity == null) {
+            throw new IllegalStateException("Static Resource not found.");
+        }
+        return responseEntity;
+    }
+}
