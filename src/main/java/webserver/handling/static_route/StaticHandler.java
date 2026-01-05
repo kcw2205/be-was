fc36295ff1f299208bf575a9ccd50f24f7a9e4da
blu3fishez <kcw2205@naver.com>
2026-01-06 00:39:58 +0900

    feat: 정적 파일 핸들러 부분에서 생기는 null 배제 및 코드 합병
    - 함수형 인터페이스 덕분에 하나의 클래스로 합병하여 책임을 통합할 수 있게 되었음
    - Response Entity의 notfound 덕분에 Optional 없이 null 배제가 가능하게 되었음

---
+++ src/main/java/webserver/handling/static_route/StaticHandler.java
@@ -0,0 +1,44 @@
+package webserver.handling.static_route;
+
+import webserver.handling.ResponseEntity;
+import webserver.http.data.HttpRequest;
+import webserver.http.enums.HttpStatusCode;
+
+public class StaticHandler {
+
+    public ResponseEntity<?> handleStaticRouteRequest(HttpRequest httpRequest) {
+        // 먼저, 정적 파일 요청인지 확인
+        ResponseEntity<?> staticFileResponse = handleStaticFileRequest(httpRequest);
+
+        if (staticFileResponse.getHttpStatusCode() != HttpStatusCode.NOT_FOUND) {
+            return staticFileResponse;
+
+        }
+
+        // 그 이후에 정적 경로 라우팅이라면, 웹 디렉토리 인덱스 html 파일 반환
+        // 그럼에도 아니면 404 에러를 리턴 하도록 수행
+        return handleRouteRequest(httpRequest.getRequestURI());
+    }
+
+    private ResponseEntity<?> handleStaticFileRequest(HttpRequest httpRequest) {
+        ResponseEntity<?> response = ResponseEntity.notFound();
+
+        for (StaticFileEnum e : StaticFileEnum.values()) {
+            response = e.fetchStaticFileFromURI(httpRequest);
+
+            if (response.getHttpStatusCode() != HttpStatusCode.NOT_FOUND) {
+                return response;
+            }
+        }
+
+        return response;
+    }
+
+    private ResponseEntity<?> handleRouteRequest(String uri) {
+        if (uri.endsWith("/")) {
+            uri = uri.substring(0, uri.length() - 1);
+        }
+        uri += "/index.html";
+        return StaticFileEnum.HTML.fetchStaticFile(uri);
+    }
+}
