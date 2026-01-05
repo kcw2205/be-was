64d13d00b98d7313980a42a48bbbe51710da7352
blu3fishez <kcw2205@naver.com>
2026-01-06 00:31:04 +0900

    refactor: 메인 함수 내에서 전체 의존성을 생성하도록 개선

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,18 +1,30 @@
 package webserver;
 
+import db.impl.UserDatabaseImpl;
+import handler.UserHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.handling.RequestDispatcher;
+import webserver.handling.RequestHandleThreadExecutor;
+import webserver.handling.RequestHandlerMapping;
+import webserver.handling.static_route.StaticHandler;
+import webserver.http.HttpRequestParser;
+import webserver.http.HttpResponseFactory;
+import webserver.http.enums.HttpRequestMethod;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import db.impl.UserDatabaseImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static final int CORE_POOL_SIZE = 2;
+    private static final int MAX_POOL_SIZE = 4;
+    private static final int QUEUE_CAPACITY = 16;
+    private static final long KEEP_ALIVE_TIME = 3;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -36,19 +48,31 @@ public class WebServer {
         }
     }
 
+    // DI 해준 다음 쓰레드풀 실행객체 반환
     private static RequestHandleThreadExecutor getRequestHandleThreadExecutor() {
-        int corePoolSize = 2;
-        int maxPoolSize = 4;
-        int queueCapacity = 16;
-        long keepAliveTime = 3;
+        var httpRequestParser = new HttpRequestParser();
+        var httpResponseFactory = new HttpResponseFactory();
 
         var userDatabase = new UserDatabaseImpl();
         var threadPoolExecutor = new ThreadPoolExecutor(
-                corePoolSize, maxPoolSize, keepAliveTime,
-                TimeUnit.SECONDS,
-                new ArrayBlockingQueue<>(queueCapacity),
-                new ThreadPoolExecutor.AbortPolicy());
+            CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+            new ThreadPoolExecutor.AbortPolicy());
 
-        return new RequestHandleThreadExecutor(threadPoolExecutor, userDatabase);
+        // static handlers
+        var staticHandler = new StaticHandler();
+        var requestHandlerMapping = new RequestHandlerMapping(staticHandler);
+
+        // user-defined handlers
+        var userHandler = new UserHandler(userDatabase);
+
+        // request dispatcher
+        var requestDispatcher = new RequestDispatcher(httpResponseFactory, requestHandlerMapping);
+
+        // handler-mapping
+        requestHandlerMapping.registerRequestHandler("/user/create", HttpRequestMethod.GET, userHandler::createUser);
+
+        return new RequestHandleThreadExecutor(threadPoolExecutor, httpRequestParser, httpResponseFactory, requestDispatcher);
     }
 }
