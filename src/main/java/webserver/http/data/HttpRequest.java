7cf88826c2234586b6ba1eae3adbcde9050a5ea4
blu3fishez <kcw2205@naver.com>
2026-01-07 00:24:21 +0900

    feat: `HttpRequest` 에 `HttpRequestBody` 와 쿠키 파싱 기능 추가
    - `HttpRequestBody` 를 적용
    - 쿠키를 파싱하여 쿠키 데이터를 가져올 수 있도록 개선
    - 필요에 의해 HTTP Method 를 가져올 수 있도록 getter 추가

--- src/main/java/webserver/http/data/HttpRequest.java
+++ src/main/java/webserver/http/data/HttpRequest.java
@@ -1,7 +1,9 @@
 package webserver.http.data;
 
+import webserver.http.enums.HttpHeaderKey;
 import webserver.http.enums.HttpRequestMethod;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
@@ -9,8 +11,9 @@ public class HttpRequest {
     private final String httpVersion;
     private final String requestURI;
     private final Map<String, String> headers;
+    private final Map<String, String> cookies;
     private final Map<String, String> queryParameters;
-    private final HttpBody body;
+    private final HttpRequestBody body;
 
     public HttpRequest(
         HttpRequestMethod requestMethod,
@@ -18,14 +21,26 @@ public class HttpRequest {
         String httpVersion,
         Map<String, String> headers,
         Map<String, String> queryParameters,
-        HttpBody body
+        HttpRequestBody body
     ) {
         this.requestMethod = requestMethod;
         this.httpVersion = httpVersion;
         this.requestURI = requestURI;
         this.headers = headers;
+        this.cookies = parseCookies(headers.getOrDefault(HttpHeaderKey.COOKIE, ""));
         this.queryParameters = queryParameters;
         this.body = body;
+    }
+
+    private Map<String, String> parseCookies(String str) {
+        Map<String, String> map = new HashMap<>();
+        for (String t : str.split(";")) {
+            String[] cookiePair = t.trim().split("=", 2);
+            if (cookiePair.length < 2) continue;
+            map.put(cookiePair[0].trim(), cookiePair[1].trim());
+        }
+
+        return map;
     }
 
     public String getHttpVersion() {
@@ -41,7 +56,7 @@ public class HttpRequest {
         return this.headers.getOrDefault(attributeName, null);
     }
 
-    public HttpBody getBody() {
+    public HttpRequestBody getBody() {
         return body;
     }
 
@@ -51,5 +66,9 @@ public class HttpRequest {
 
     public Map<String, String> getQueryParameters() {
         return queryParameters;
+    }
+
+    public HttpRequestMethod getHttpMethod() {
+        return this.requestMethod;
     }
 }
