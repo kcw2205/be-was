2515f215f6413baddeb9dd8107007e67f9c0b8d1
blu3fishez <kcw2205@naver.com>
2026-01-07 00:29:05 +0900

    feat: 정적 핸들러에서 `GET` 요청인지 확인하는 로직 추가

--- src/main/java/webserver/handling/static_route/StaticHandler.java
+++ src/main/java/webserver/handling/static_route/StaticHandler.java
@@ -2,21 +2,25 @@ package webserver.handling.static_route;
 
 import webserver.handling.ResponseEntity;
 import webserver.http.data.HttpRequest;
+import webserver.http.enums.HttpRequestMethod;
 import webserver.http.enums.HttpStatusCode;
 
 public class StaticHandler {
 
     public ResponseEntity<?> handleStaticRouteRequest(HttpRequest httpRequest) {
+        if (httpRequest.getHttpMethod() != HttpRequestMethod.GET) {
+            return ResponseEntity.notFound();
+        }
+        
         // 먼저, 정적 파일 요청인지 확인
         ResponseEntity<?> staticFileResponse = handleStaticFileRequest(httpRequest);
 
         if (staticFileResponse.getHttpStatusCode() != HttpStatusCode.NOT_FOUND) {
             return staticFileResponse;
-
         }
 
         // 그 이후에 정적 경로 라우팅이라면, 웹 디렉토리 인덱스 html 파일 반환
-        // 그럼에도 아니면 404 에러를 리턴 하도록 수행
+        // 그럼에도 아니면 404 에러)를 리턴 하도록 수행
         return handleRouteRequest(httpRequest.getRequestURI());
     }
 
