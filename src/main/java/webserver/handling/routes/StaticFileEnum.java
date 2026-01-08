e22e51dbd06cc113b88894b06f388c8d5e646b66
blu3fishez <kcw2205@naver.com>
2026-01-08 17:12:25 +0900

    feat: StaticFileResolver 추가하여 정적 파일 자체를 찾아서 반환해주는 로직을 분리하여 재활용하도록 개선

---
+++ src/main/java/webserver/handling/routes/StaticFileEnum.java
@@ -0,0 +1,34 @@
+package webserver.handling.routes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.http.enums.HttpContentType;
+
+public enum StaticFileEnum {
+
+    HTML(".html", HttpContentType.HTML),
+    CSS(".css", HttpContentType.CSS),
+    JS(".js", HttpContentType.JS),
+    PNG(".png", HttpContentType.PNG),
+    ICO(".ico", HttpContentType.ICO),
+    JPG(".jpg", HttpContentType.JPG),
+    SVG(".svg", HttpContentType.SVG),
+    ;
+
+    private static final Logger logger = LoggerFactory.getLogger(StaticFileEnum.class);
+    private final String ext;
+    private final HttpContentType contentType;
+
+    StaticFileEnum(String ext, HttpContentType contentType) {
+        this.ext = ext;
+        this.contentType = contentType;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public HttpContentType getContentType() {
+        return contentType;
+    }
+}
