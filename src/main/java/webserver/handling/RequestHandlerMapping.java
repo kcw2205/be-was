effbf45b5b2195ca2b647c2a01d7a9a90e26f46f
blu3fishez <kcw2205@naver.com>
2026-01-13 16:22:14 +0900

    feat: 웹서버에서 Method Not Allowed 예외 반환 기능 추가

--- src/main/java/webserver/handling/RequestHandlerMapping.java
+++ src/main/java/webserver/handling/RequestHandlerMapping.java
@@ -1,8 +1,10 @@
 package webserver.handling;
 
 import webserver.handling.statics.StaticHandler;
+import webserver.http.HttpException;
 import webserver.http.data.HttpRequest;
 import webserver.http.enums.HttpRequestMethod;
+import webserver.http.enums.HttpStatusCode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,21 +17,42 @@ import java.util.Map;
  * 정적 핸들러를 반환하도록 하는 것도 해당 매핑 클래스의 역할아닌가? 싶어서 일단 넣어봤다.
  */
 public class RequestHandlerMapping {
+
     private final StaticHandler staticHandler;
-    // TODO: 405 (경로는 있으나, Method는 없는 경우) 를 지원해주어야함
-    private final Map<String, RequestHandler> requestHandlerMap = new HashMap<>();
+    private final Map<String, Map<HttpRequestMethod, RequestHandler>> requestHandlerMap = new HashMap<>();
 
     public RequestHandlerMapping(StaticHandler staticHandler) {
         this.staticHandler = staticHandler;
     }
 
-    public void registerRequestHandler(String URI, HttpRequestMethod httpRequestMethod, RequestHandler interceptor) {
-        this.requestHandlerMap.put(httpRequestMethod.toString() + " " + URI, interceptor);
+    public void registerRequestHandler(String URI, HttpRequestMethod httpRequestMethod, RequestHandler handler) {
+        Map<HttpRequestMethod, RequestHandler> handlerMap = this.requestHandlerMap.getOrDefault(URI, null);
+
+        if (handlerMap == null) {
+            this.requestHandlerMap.put(URI, new HashMap<>());
+            handlerMap = this.requestHandlerMap.get(URI);
+        }
+
+        handlerMap.put(httpRequestMethod, handler);
     }
 
     public RequestHandler getRequestHandler(HttpRequest httpRequest) {
-        String fullRequestURI = httpRequest.getHttpMethod().toString() + " " + httpRequest.requestURI();
+        HttpRequestMethod method = httpRequest.requestMethod();
+        String uri = httpRequest.requestURI();
 
-        return this.requestHandlerMap.getOrDefault(fullRequestURI, staticHandler::handleStaticRouteRequest);
+        Map<HttpRequestMethod, RequestHandler> handlerMap =
+            this.requestHandlerMap.getOrDefault(uri, null);
+
+        if (handlerMap == null) {
+            return staticHandler::handleStaticRouteRequest;
+        }
+
+        RequestHandler requestHandler = handlerMap.get(method);
+
+        if (requestHandler == null) {
+            throw new HttpException(HttpStatusCode.METHOD_NOT_ALLOWED, HttpStatusCode.METHOD_NOT_ALLOWED.getStatusName());
+        }
+
+        return requestHandler;
     }
 }
