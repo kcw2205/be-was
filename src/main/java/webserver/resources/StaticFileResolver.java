f0aa89e2723e7e0b16e80357df21ec6c55419d65
blu3fishez <kcw2205@naver.com>
2026-01-15 00:46:16 +0900

    feat: 이미지 업로드 기능을 위해 업로드 디렉토리도 리소스 검색 범위에 추가

--- src/main/java/webserver/resources/StaticFileResolver.java
+++ src/main/java/webserver/resources/StaticFileResolver.java
@@ -1,12 +1,23 @@
-package webserver.handling.statics;
+package webserver.resources;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.util.Optional;
 
 public class StaticFileResolver {
 
     public Optional<byte[]> fetchStaticFile(String resourcePath) {
+        try {
+            File externalFile = new File(ResourcePath.EXTERNAL_STORAGE.getPath(), resourcePath);
+            if (externalFile.exists() && externalFile.isFile()) {
+                return Optional.of(Files.readAllBytes(externalFile.toPath()));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         try (InputStream resourceStream = getClass().getClassLoader().getResourceAsStream("static/" + resourcePath)) {
             if (resourceStream == null) {
                 return Optional.empty();
